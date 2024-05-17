1302d36d1816758c7a9a42103557fb709b83f6cb
jayanth <165253471+gopuJayanth@users.noreply.github.com>
2024-05-17 21:23:30 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const a = document.querySelector("names");
+
+function btn(input){
+    names.value += input
+}
+function equal(){
+    names.value = eval(names.value)
+
+}
+
+
+function Clear(){
+    names.value = "";
+}
